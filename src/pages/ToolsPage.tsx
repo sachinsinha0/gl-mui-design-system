1512b9aa3b4a7c366840780ee6694aed7408910b
sachinsinha0 <sachin.sinha@greatlearning.in>
2026-04-28 16:01:21 +0530

    Bump Design Mirror chip to v1.4.0

--- src/pages/ToolsPage.tsx
+++ src/pages/ToolsPage.tsx
@@ -202,7 +202,7 @@ export default function ToolsPage() {
                     Design Mirror
                   </Typography>
                   <Chip
-                    label="v1.3.8"
+                    label="v1.4.0"
                     size="small"
                     color="primary"
                     variant="outlined"
